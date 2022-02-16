b2ee435f846e046a7d51452be1cd34c1381a1c10
alanchen-1 <alan_chen1@brown.edu>
2022-02-16 17:20:38 -0500

    add multiple iterations of mcmc

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from libs.datalib import *
 from libs.mcmclib import *
 import string
 
-INFILE = "data/corpus/crime-and-punishment.txt"
-OUTFILE = "data/corpus/crime-and-punishment-clean.txt"
-IN_CIPHERTEXT = "data/test/h_5.txt"
+INFILE = "data/corpus/war-and-peace.txt"
+OUTFILE = "data/corpus/war-and-peace-clean.txt"
+IN_CIPHERTEXT = "data/test/f_5.txt"
+OUT_DECODED = "./results/" + IN_CIPHERTEXT.split("/")[-1]
+print(OUT_DECODED)
+ciphertext = read_ciphertext(IN_CIPHERTEXT)
 STATE_SPACE = list(string.ascii_lowercase) + [' ']
 CONVERGENCE = 5000
 
@@ -47,7 +50,7 @@ def naive_forward_mcmc(ciphertext, iterations):
         if convergence_count >= CONVERGENCE:
             break;
     
-    return permutation
+    return permutation, naive_likelihood(ciphertext, permutation, char_freqs, transition_matrix, letter_dict)
 
 def fast_forward_mcmc(iterations):
     ciphertext_counts = precomp_ciphertext(IN_CIPHERTEXT, letter_dict)
@@ -74,17 +77,31 @@ def fast_forward_mcmc(iterations):
         if convergence_count >= CONVERGENCE:
             break;
     
-    return permutation
+    return permutation, fast_likelihood(ciphertext_counts, permutation, transition_matrix, letter_dict)
 
-OUT_DECODED = "./results/" + IN_CIPHERTEXT.split("/")[-1]
-print(OUT_DECODED)
-ciphertext = read_ciphertext(IN_CIPHERTEXT)
+def full_mcmc(iterations, each_iterations=50000):
+    best_energy = float('inf')
+    best_permutation = Permutation()
+    for i in range(iterations):
+        print("Try", i + 1)
+        permute, energy = fast_forward_mcmc(each_iterations)
+        if(energy < best_energy):
+            best_energy = energy
+            best_permutation = permute
+    
+    print("Best permutation found:", best_permutation.map_dict)
+    print("Sample decoded:", decode_ciphertext(ciphertext, best_permutation, length = 100))
+    return best_permutation
+
+
 #final_permute = naive_forward_mcmc(ciphertext, 25000)
-final_permute = fast_forward_mcmc(25000)
+#final_permute, energy = fast_forward_mcmc(25000)
+final_permute = full_mcmc(10, each_iterations=25000)
 
 print("Writing to", OUT_DECODED)
 with open(OUT_DECODED, 'w+', encoding='utf8') as out:
-    out.write("Final permutation: \n" + final_permute.to_string())
+    out.write("Final permutation: \n" + final_permute.to_string() + "\n")
+    out.write("Scrambled text: \n" + ciphertext + "\n")
     out.write("Unscrambled text: \n" + decode_ciphertext(ciphertext, final_permute))
     
 
